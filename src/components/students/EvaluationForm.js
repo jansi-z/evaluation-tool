9e4a4bd7d4b7148872f10d6f2e3d516b4bd4ae18
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-11 14:27:07 +0200

    Input evaluation form is taken and saved to constants

--- src/components/students/EvaluationForm.js
+++ src/components/students/EvaluationForm.js
@@ -9,32 +9,45 @@ import './EvaluationForm.css'
 class EvaluationForm extends PureComponent {
 
   state = {
-    value: 1,
+    value: "green",
   };
 
   handleChange = (event, index, value) => this.setState({value});
+
+  submitEvaluation(event){
+    event.preventDefault()
+    // debugger
+    const date = this.refs.date.refs.input.input.value
+    const color = this.refs.color.props.value
+    const remarks = this.refs.remarks.getValue()
+    const author = this.props.currentUser._id
+    const evaluationData = { date: date, color: color, remark: remarks, author: author }
+  }
 
   render() {
 
     return (
       <div className="evaluation">
         <h2>Evaluate student</h2>
-        <form id="evaluationForm">
+        <form id="evaluationForm" onSubmit={ this.submitEvaluation.bind(this) }>
           <DatePicker
-            hintText="Date" />
+            hintText="Date"
+            ref="date" />
           <SelectField
             value={this.state.value}
             onChange={this.handleChange}
+            ref="color"
           >
-            <MenuItem value={1} primaryText="Green" />
-            <MenuItem value={2} primaryText="Yellow" />
-            <MenuItem value={3} primaryText="Red" />
+            <MenuItem value={"green"} primaryText="Green" />
+            <MenuItem value={"yellow"} primaryText="Yellow" />
+            <MenuItem value={"red"} primaryText="Red" />
           </SelectField>
           <TextField
             hintText="Remarks"
             floatingLabelText="Remarks:"
             ref="remarks"
           />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     )
